cfa576cbeb55510d711d8b9fc191eac27cef1bb2
hilkiasmoraes <hilkias.moraes@gmail.com>
2026-06-20 12:02:28 -0300

    feat: menu dinâmico com objetos

---
+++ src/js/components/menu/itens.js
@@ -0,0 +1,21 @@
+//    URL   :  LABEL
+const itens_menu = [
+    {
+        url:'#inicio',
+        label:'Inicio'
+    },
+    {
+        url:'#sobre',
+        label:'Sobre'
+    },
+    {
+        url:'#servicos',
+        label:'Serviços'
+    },
+    {
+        url:'#contato',
+        label:'Contato'
+    }
+]
+
+export default itens_menu;
